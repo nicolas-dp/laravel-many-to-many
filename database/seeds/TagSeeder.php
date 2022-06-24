c9c7b30b11e155e7cae8759b4b2527258b3e9586
Nicolas De Palma <wellcomenicolas@gmail.com>
2022-06-24 17:02:00 +0200

    Add Tag Seeder

---
+++ database/seeds/TagSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+use App\Models\Tag;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
+
+class TagSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $tags = ['Learn','DAD','SW','Booleaner','Laravel','Financial'];
+        foreach ($tags as $tag) {
+            $new_tag = new Tag();
+            $new_tag->name = $tag;
+            $new_tag->slug = Str::slug($new_tag->name);
+            $new_tag->save();
+        }
+    }
+}
